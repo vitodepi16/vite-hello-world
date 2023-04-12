dc9238132f5f35dd73a65bbac2fa2b121e161cc6
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-04-12 14:47:34 +0200

    cut element + print titol in index

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ titolo }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AppTitle',
+    data(){
+      return{
+        titolo: 'La mia prima app con Vite!'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
